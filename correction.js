9c5196f80896a10727a6c080bcf166996f9a4285
wassime akachar <wassime.akachar@hotmail.com>
2021-11-08 14:39:45 +0100

    finis

---
+++ correction.js
@@ -0,0 +1,22 @@
+//1 
+let redPurple = document.getElementsByClassName('redPurple')
+console.log(redPurple);
+//2
+let redPurple2 = document.querySelectorAll('.redPurple')
+console.log(redPurple2);
+
+//3
+let recupH1 = document.querySelector('h1.redPurple')
+console.log(recupH1);
+
+//4
+let exo4 = document.querySelectorAll('li,p,span')
+console.log(exo4);
+
+//5
+let exo5 = document.querySelectorAll('li.important,p')
+console.log(exo5);
+
+//6
+let exo6 = document.querySelectorAll('h1.redPurple,span.redPurple')
+console.log(exo6);
